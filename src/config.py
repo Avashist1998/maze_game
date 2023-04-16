ca0937d9a75c037984ffcc1ff3419a0812c23047
Avashist1998 <36799410+Avashist1998@users.noreply.github.com>
2023-04-15 20:17:26 -0500

    Config Setup (#12)
    * feat: pipeline~
    
    * feat: config addition
    
    * resolved

---
+++ src/config.py
@@ -0,0 +1,38 @@
+"""Defines the config class for the maze game."""
+import os
+
+class Config:
+    """Defines the config class for the maze game."""
+
+    DEBUG = False
+    APP_NAME = "Maze Game"
+    APP_VERSION = "1.0.0"
+    APP_DESCRIPTION = "A maze game."
+    APP_AUTHOR = "The Maze Game Team"
+    APP_CONFIG = "CONFIG"
+    LOG_LEVEL = "INFO"
+    LOG_FILE = "maze_game.log"
+    LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+class BaseConfig(Config):
+    """Defines the base config class for the maze game."""
+
+    TWITCH_MODE = False
+    APP_CONFIG = "BASE_CONFIG"
+
+class TwitchConfig(Config):
+    """Defines the twitch config class for the maze game."""
+
+    APP_CONFIG = "TWITCH_CONFIG"
+    TWITCH_MODE = os.environ.get("TWITCH_MODE", False)
+    TWITCH_CHANNEL = os.environ.get("TWITCH_CHANNEL", "")
+    TWITCH_USERNAME = os.environ.get("TWITCH_USERNAME", "")
+    TWITCH_OAUTH_TOKEN = os.environ.get("TWITCH_OAUTH_TOKEN", "")
+
+def get_config() -> Config:
+    """Returns the config object."""
+    if TwitchConfig.TWITCH_MODE:
+        return TwitchConfig # type: ignore
+    return BaseConfig # type: ignore
+ 
--- src/logger.py
+++ src/logger.py
@@ -1,23 +1,30 @@
 """Maze Game Logger definition."""
 from typing import Final
-from logging import INFO, getLogger, FileHandler, StreamHandler, Formatter, Logger
+from logging import INFO, DEBUG, getLogger, FileHandler, StreamHandler, Formatter, Logger
 
+from src.config import get_config
 
 def init_logger(name: str) -> Logger:
     """Initialize the module logger."""
-
+    config = get_config()
+    print(config)
     logger: Final = getLogger(name)
     logger.setLevel(INFO)
+    if config.DEBUG:
+        logger.setLevel(DEBUG)
 
     # create file handler which logs even debug messages
-    file_handler = FileHandler('run_logs.log')
+    file_handler = FileHandler(config.LOG_FILE)
     file_handler.setLevel(INFO)
+    if config.DEBUG:
+        file_handler.setLevel(DEBUG)
     # create console handler with a higher log level
     console_handler = StreamHandler()
     console_handler.setLevel(INFO)
+    if config.DEBUG:
+        console_handler.setLevel(DEBUG)
     # create formatter and add it to the handlers
-    formatter = Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = Formatter(config.LOG_FORMAT, datefmt=config.LOG_DATE_FORMAT)
 
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
